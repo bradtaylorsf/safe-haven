cec8feb9ff8444d7fed016270ae95979f61e69b0
darkyin87 <darkyin87@gmail.com>
2017-08-10 16:46:45 -0700

    Adding more empty components for routes

---
+++ app/components/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logout = () =>
+  (
+    <div>
+      Logout
+    </div>
+  );
+
+
+export default Logout;
--- app/routes.js
+++ app/routes.js
@@ -3,14 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import Register from './components/Register';
+import Logout from './components/Logout';
+import Shelters from './components/Shelters';
 
 export default (
   <Switch>
     <Route exact path = "/" component = {App} />
     <Route path = "/login" component = {Login} />
     <Route path = "/profile" component = {Profile} />
-    <Route path = "/register" component = {Profile} />
-    <Route path = "/logout" component = {Profile} />
-    <Route path = "/shelter" component = {Profile} />
+    <Route path = "/register" component = {Register} />
+    <Route path = "/logout" component = {Logout} />
+    <Route path = "/shelter" component = {Shelters} />
   </Switch>
 );
